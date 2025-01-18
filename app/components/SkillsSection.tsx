705cef61b6a7aad92636ad16ab906ffd23758325
Zafir Ali <jimmyurkel@gmail.com>
2025-01-17 21:27:11 -0500

    slight changes, added more space between projects and skills section

--- app/components/SkillsSection.tsx
+++ app/components/SkillsSection.tsx
@@ -1,5 +1,4 @@
-'use client'
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 import { useMemo } from 'react'
 
 const skills = ['React', 'Next.js', 'JavaScript', 'TypeScript', 'Tailwind CSS', 'Node.js', 'C', 'R', 'Java']
@@ -8,18 +7,16 @@ const SkillsSection = () => {
   const memoizedSkills = useMemo(() => skills, [])
 
   return (
-    <motion.section 
-      className="py-20 pt-32 text-white"
+   <motion.section 
+      className="py-20 pt-48 text-white"
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
-      transition={{ duration: 1 }}
       viewport={{ once: true }}
     >
       <motion.h2 
         className="text-3xl font-bold mb-8 text-center text-white"
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
         viewport={{ once: true }}
       >
         Skills
@@ -31,9 +28,7 @@ const SkillsSection = () => {
             className="bg-white bg-opacity-10 backdrop-filter px-4 py-2 rounded-full shadow-md text-white"
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.1}} 
             viewport={{ once: true }}
-            whileHover={{ scale: 1.05 }} 
           >
             {skill}
           </motion.div>
@@ -44,4 +39,3 @@ const SkillsSection = () => {
 }
 
 export default SkillsSection
-
