ca6694e4b330f3f94f1b6afc573e488a1f4f4b04
Zafir Ali <jimmyurkel@gmail.com>
2025-02-03 01:51:25 -0500

    background update, inshallah works

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import Head from "next/head"
+import { Squares } from "./components/ui/squares-background"
 import type React from "react"
 
 export const metadata = {
@@ -20,9 +22,13 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className="min-h-screen flex flex-col relative">
+        <div className="fixed inset-0 z-0">
+          <Squares direction="diagonal" speed={0.5} squareSize={40} borderColor="#333" hoverFillColor="#222" />
+        </div>
         <Header />
         <main className="flex-grow pl-16 relative z-10">{children}</main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   )
